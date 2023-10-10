04c6da860319832f8429bb81c681d93ce223fbfa
Ahmadullo26062007 <ahmadullo0039@gmail.com>
2023-10-10 18:57:13 +0500

    admin user finished

---
+++ resources/views/admin/usermenegment/roles/create.blade.php
@@ -0,0 +1,68 @@
+@extends('layout.admin')
+@section('content')
+    <div class="container-fluid pt-4 px-4">
+        <div class="row g-4">
+            <div class="col-12">
+                <div class="bg-secondary rounded h-100 p-4">
+                    <h6 class="mb-4">User Add</h6>
+                    <form action="{{route('users.store')}}" method="post">
+                        @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                                <div class="alert alert-danger" role="alert">
+                                    {{$error}}
+                                </div>
+                            @endforeach
+                        @endif
+                        @csrf
+                        <div class="form-floating mb-3">
+                            <input type="text" name="name" class="form-control text-white" id="floatingInput"
+                                   placeholder="">
+                            <label for="floatingInput">Name</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="number" name="phone_number" class="form-control text-white" id="floatingInput"
+                                   placeholder="">
+                            <label for="floatingInput">Phone Number</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" name="password" class="form-control" id="floatingPassword"
+                                   placeholder="Password">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="email" name="email" class="form-control text-white" id="floatingInput"
+                                   placeholder="name@example.com">
+                            <label for="floatingInput">Email address(Nullable)</label>
+                        </div>
+                        <div>
+                            <select class="form-select" name="roles" multiple id="floatingSelect"
+                                    aria-label="Floating label select example">
+                                @foreach($roles as $id=>$item)
+                                    <option value="{{$id}}">{{$item}}</option>
+                                @endforeach
+                            </select>
+                            <label for="floatingSelect">Works with selects</label>
+                        </div>
+                        <button class="btn btn-primary mt-3" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                <path fill-rule="evenodd"
+                                      d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+                            </svg>
+                            Create
+                        </button>
+                        <a class="btn btn-secondary mt-3" href="{{route('users.index')}}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                      d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                            </svg>
+                            Back
+                        </a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
