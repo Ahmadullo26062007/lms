05eb0d406f9e80cc02808210679c510c38a565bc
Yahyobek <mywhite060608@gmail.com>
2023-10-11 20:02:49 +0500

    created new livewire coimponent

---
+++ app/Livewire/StudentLivewire.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class StudentLivewire extends Component
+{
+    public function render()
+    {
+        return view('livewire.student-livewire');
+    }
+}
