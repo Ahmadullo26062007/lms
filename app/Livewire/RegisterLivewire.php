855d15e5724b569c15845dc5ce9b76f748ffd3e2
Ahmadullo26062007 <ahmadullo0039@gmail.com>
2023-10-11 20:02:21 +0500

    web

---
+++ app/Livewire/RegisterLivewire.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class RegisterLivewire extends Component
+{
+    public function render()
+    {
+        return view('livewire.register-livewire');
+    }
+}
--- routes/auth.php
+++ routes/auth.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login5', function () {
+Route::get('/login', function () {
 return view('auth.signin');
-})->name('login5');
+})->name('login');
 
 Route::get('/register', function () {
 return view('auth.signup');
