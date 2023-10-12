5a4cd223c4eecd7a1b03030026bd0a3721666dd4
abduraxmon <abduraxmon.jalolov12@gmail.com>
2023-10-12 16:37:17 +0500

    dscsd

--- database/migrations/2023_10_10_095351_create_students_table.php
+++ database/migrations/2023_10_10_095351_create_students_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger("user_id");
             $table->unsignedBigInteger("role_id");
-            
+
             $table->string("first_name");
             $table->string("last_name");
             $table->integer("phone_number");
 
-            $table->foreign("user_id")->references("id")->on("users");
             $table->foreign("role_id")->references("id")->on("roles");
             $table->timestamps();
         });
